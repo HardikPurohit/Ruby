dde67e71ded72c9742c54691165ec8ea42a9a894
HardikPurohit <hardik.purohit165@gmail.com>
2017-01-13 16:49:11 +0530

    Sum of multiple

---
+++ sum_of_multiples.rb
@@ -0,0 +1,29 @@
+class SumOfMultiples
+
+  def initialize(*num)
+    @array_of_numbers = num
+  end
+
+  def to(multiple_limit)
+    @result_array = []
+    sum_of_multiples=0
+    (0..@array_of_numbers.length-1).each do |i|
+      if multiple_limit > @array_of_numbers[i]
+        j = 1
+        while (@array_of_numbers[i] * j) < multiple_limit
+          a = @array_of_numbers[i] * j
+          if !@result_array.include?(a)
+            @result_array.push(a)
+          end
+          j += 1
+        end
+      else
+        sum_of_multiples = 0
+      end
+    end
+    (0..@result_array.length-1).each do |i|
+      sum_of_multiples += @result_array[i]
+    end
+    return sum_of_multiples
+  end
+end
