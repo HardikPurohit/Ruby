0d0ab047068df564b12378fe09455c96c8d105c6
HardikPurohit <hardik.purohit165@gmail.com>
2017-01-13 12:36:59 +0530

    word count

---
+++ word_count.rb
@@ -0,0 +1,36 @@
+class Phrase
+
+  def initialize(phrase)
+    @phrase_to_count=phrase
+    @phrase_array = @phrase_to_count.scan(/\w+(?:'\w+)*/)
+    @new_phrase = @phrase_array.join(" ")
+    @new_phrase.gsub!',',' '
+    @new_phrase.gsub!':',' '
+    @new_phrase.gsub!(/[!@.%&$^"]/,'')
+    @new_phrase.downcase!
+  end
+
+  def word_count
+    array_of_phrase=@new_phrase.split(" ")
+    phrase = Hash.new
+    count_occurrence = 1
+    (0..array_of_phrase.length-1).each do |i|
+      if phrase.key?(array_of_phrase[i])
+        next
+      end
+      (i+1..array_of_phrase.length-1).each do |j|
+        if array_of_phrase[i] == array_of_phrase[j]
+         count_occurrence += 1
+        end
+      end
+      phrase[array_of_phrase[i]] = count_occurrence
+      count_occurrence = 1
+    end
+    return phrase
+  end
+
+end
+
+module BookKeeping
+  VERSION = 1
+end
